63d2acae7aea4400e984c9208435712036c1894c
GauravNautiyal1 <nautiyalgaurav077@gmail.com>
2025-09-29 11:44:47 +0530

    Uploaded all files at once

--- minnorproject/sumoffive.c
+++ minnorproject/sumoffive.c
@@ -45,16 +45,63 @@
 
 
 
-#include <stdio.h>
-int a=5;
-int factorial=1;
-int main(){
- for(int i=1;i<=a;i++){
-factorial=i*factorial;
+// #include <stdio.h>
+// int a=5;
+// int factorial=1;
+// int main(){
+//  for(int i=1;i<=a;i++){
+// factorial=i*factorial;
 
    
-    }
-    printf("\nThe factorial of %d is %d",a,factorial);
- }
+//     }
+//     printf("\nThe factorial of %d is %d",a,factorial);
+//  }
+
+
+// #include <stdio.h>
+// int arr[10];
+// int largestnumber;
+// int smallestnumber;
+// int main(){
+//     printf("Enter 10 numbers:\n");
+//     for(int i=0;i<10;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     largestnumber=arr[0];
+//     smallestnumber=arr[0];
+//     for(int i=1;i<10;i++){
+//         if(arr[i]>largestnumber){
+//             largestnumber=arr[i];
+//         }
+//         if(arr[i]<smallestnumber){
+//             smallestnumber=arr[i];
+//         }
+//     }
+//     printf("The largest number is %d\n",largestnumber);
+//     printf("The smallest number is %d\n",smallestnumber);
+//     return 0;
+// }
+
+
+
+#include <stdio.h> 
+#include <stdlib.h>
+int sum;
+double average;
+int main() {
+int *arr=(int *)malloc(10*sizeof(int));
+printf("Enter 10 numbers:\n");
+scanf("%d",&arr[0]);
+for(int i=1;i<10;i++){
+    scanf("%d",&arr[i]);
+   }
+   for(int i=0;i<10;i++){
+  sum = sum + arr[i];
+   }
+   average=(double)sum/10;
+   printf("The sum of 10 numbers is %d\n",sum);
+   printf("The average of 10 numbers is %.2f\n",average);
+   free(arr);
+}
 
 
